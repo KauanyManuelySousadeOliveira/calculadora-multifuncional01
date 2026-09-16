e3663db30366f4d330f5c3e8691019bf193c9b2c
KAUANY MANUELY SOUSA DE OLIVEIRA <00001119772886sp@aluno.educacao.sp.gov.br>
2026-09-16 07:33:43 -0300

    feat: adicionar multiplicacao e divisao

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-function somar() {
-    let numero1 = Number(document.getElementById("numero1").value);
-    let numero2 = Number(document.getElementById("numero2").value);
-
-    let resultado = numero1 + numero2;
-
-    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+function multiplicar() {
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+  document.getElementById("resultado").innerText =
+    "Resultado: " + numero1 * numero2;
 }
 
-function subtrair() {
-    let numero1 = Number(document.getElementById("numero1").value);
-    let numero2 = Number(document.getElementById("numero2").value);
+function dividir() {
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
 
-    let resultado = numero1 - numero2;
-
-    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  if (numero2 === 0) {
+    document.getElementById("resultado").innerText =
+      "Resultado: Não é possível dividir por zero.";
+    return;
+  }
+  document.getElementById("resultado").innerText =
+    "Resultado: " + numero1 / numero2;
 }
